7452da4984cb98d569b818bb041b14995c410682
ELMESKINEAnas <78366209+ELMESKINEAnas@users.noreply.github.com>
2021-02-19 22:33:42 +0100

    Initial commit

---
+++ slide.js
@@ -0,0 +1,18 @@
+    var swiper = new Swiper('.swiper-container', {
+      spaceBetween: 30,
+      centeredSlides: true,
+      // autoplay: {
+      //   delay: 3000,
+      //   disableOnInteraction: true,
+      // },
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+    });
+
+
